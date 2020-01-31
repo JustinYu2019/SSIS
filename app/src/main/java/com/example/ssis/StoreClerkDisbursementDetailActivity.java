7bf6e99845505fc076d7d768d076c2179b23e0bf
JustinYu2019 <e0003064@u.nus.edu>
2020-01-31 13:45:45 +0800

    Store Clerk Ui done, left some control element undone

--- app/src/main/java/com/example/ssis/StoreClerkDisbursementDetailActivity.java
+++ app/src/main/java/com/example/ssis/StoreClerkDisbursementDetailActivity.java
@@ -3,12 +3,28 @@ package com.example.ssis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class StoreClerkDisbursementDetailActivity extends AppCompatActivity {
-
+String name;String collectionPoint;String contactNumber;String deptRep;String reqId;
+    ArrayList<Item>itemList;
+    LinearLayout dChartLayout;
+    TableLayout dTableLayout;
+    TextView desc;
+    EditText quanEdit;
+    EditText reasonEdit;
+    TextView qty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +32,64 @@ public class StoreClerkDisbursementDetailActivity extends AppCompatActivity {
 
         Intent i=getIntent();
         Department d=(Department) i.getSerializableExtra("Department");
+        name=d.getDeptName();
+        collectionPoint=d.getCollectionPoint();
+        contactNumber=d.getContactNumber();
+        deptRep=d.getDeptRep();
+        reqId=d.getRequisitionId();
 
-        // try to use adapter to do and see if it allows bottom space.
 
+        // set up confirm Button, upon click, will send notification to the clerk..--> this needs to learn..
+
+
+        // use table layout
+        //itemList=new ArrayList<>();
+        itemList=d.getItemList();
+        dChartLayout=findViewById(R.id.chart_layoutDisburse);
+        dTableLayout=new TableLayout(StoreClerkDisbursementDetailActivity.this);
+        displayCharTableDisburse();
+    }
+
+    public void displayCharTableDisburse(){
+        dTableLayout.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT)
+        );
+
+        for (Item item: itemList){
+            final TableRow row=new TableRow(StoreClerkDisbursementDetailActivity.this);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT
+                    ,TableRow.LayoutParams.WRAP_CONTENT));
+
+            String description=item.getDescription();
+            int quantity=item.getUnit();
+            desc= new TextView(StoreClerkDisbursementDetailActivity.this);
+
+            desc.setText("  "+description);
+            desc.setTextColor(Color.BLACK);
+            desc.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT
+            ));
+            desc.setPaddingRelative(0,0,30,0);
+            row.addView(desc);
+
+            qty=new TextView(StoreClerkDisbursementDetailActivity.this);
+            qty.setText("Qty: ");
+            //qty.setPaddingRelative(0,0,30,0);
+            row.addView(qty);
+
+            //TextView valueTV2= new TextView(StoreClerkDisbursementDetailActivity.this);
+            quanEdit=new EditText(StoreClerkDisbursementDetailActivity.this);
+            quanEdit.setTextColor(Color.BLACK);
+            quanEdit.setText(""+quantity); // adjust here if quantity do not align
+            quanEdit.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT
+            ));
+            row.addView(quanEdit);
+
+
+
+            dTableLayout.addView(row);
+        }
+        dChartLayout.addView(dTableLayout);
     }
 }
